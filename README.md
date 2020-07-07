9891089d7f2695a523ae875dea38c98387db5350
Joshua Stephenson <josh@brightmediums.com>
2020-07-07 17:23:37 -0400

    First attempt at averages across values by subject and activity

--- README.md
+++
@@ -1,6 +0,0 @@
----
-title: "README"
-author: "JS"
-date: "7/6/2020"
-output: html_document
----
--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,7 @@
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 ## Downloads the data file and unzips if it doesn't exist already
-downloadData <- function() {
+downloadDataAndUnzip <- function() {
         zipfilePath <- paste("data.zip", sep = "")
         url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         if (!file.exists(zipfilePath)) {
@@ -17,21 +17,14 @@ downloadData <- function() {
 
 ## passed dataDir, type of reading (time or frequency) it returns only the mean and standard deviation columns
 ## with properly named headers for that type
-getMeanAndStandardDevData <- function(dataDir, 
-                                      type="time", 
-                                      isTest=TRUE,
-                                      signals = "Inertial Signals") {
+getMeanAndStandardDevData <- function(dataDir = "UCI HAR Dataset", isTest=TRUE) {
         
         x_filename = if(isTest) {"X_test.txt"} else {"X_train.txt"}
         y_filename = if(isTest) {"y_test.txt"} else {"y_train.txt"}
         subject_filename = if(isTest) {"subject_test.txt"} else {"subject_train.txt"}
         subfolder = if(isTest) {"test"} else {"train"}
         
-        types <- c("frequency", "time")
-        if (!(type %in% types)) {
-                stop("type options: time|frequency")
-        }
-        features <- getFeatureNames(type)
+        features <- getCleanedFeatureNames()
         
         Path.X <- paste(dataDir, "/", subfolder, "/", x_filename, sep = "")
         Data.X <- read.table(Path.X, header = F)
@@ -55,42 +48,29 @@ getMeanAndStandardDevData <- function(dataDir,
         Data.merged
         Data.X["ActivityName"] = Data.merged["Name"]
         # Data.X["ActivityID"] = Data.merged["ID"]
-        write.table(Data.X, file= "output.txt", row.names = FALSE)        
         Data.X
 }
 
 # For each type of either `time` or `frequency` open the features.txt file
 # and find all feature names that match the type that also only match mean or
 # standard deviation features
-getFeatureNames <- function(type = "time", uciHarDataset = "UCI HAR Dataset") {
+getCleanedFeatureNames <- function(uciHarDataset = "UCI HAR Dataset") {
+        
         filename <- paste(uciHarDataset, "/", "features.txt", sep="")
         if (!file.exists(filename)) {
                 stop(paste("Cannot find features.txt (", filename, ")", sep=""))
         }
         features <- read.table(filename)
         
-        types <- c("frequency", "time")
-        if (!(type %in% types)) {
-                stop("type options: time|frequency")
-        }
-        
         ## Find only the features with `mean` or `std` which represent Mean and standard deviation respectively
         selected_indices <- grep("(mean|std)", features[[2]])
         selected <- features[selected_indices,]
         
-        ## Clean up the feature names which will become column names
-        # leading `t` should become Time
-        if (type == "time") {
-                # Find the frequency readings and replace `t` with `Time`
-                selected_indices <- grep("^t", selected[[2]])
-                selected <- selected[selected_indices,]
-                selected[[2]] <- gsub("^t", "Time", selected[[2]])
-        }else if (type == "frequency") {
-                # Find the frequency readings and replace `r` with `Frequency`
-                selected_indices <- grep("^f", selected[[2]])
-                selected <- selected[selected_indices,]
-                selected[[2]] <- gsub("^f", "Frequency", selected[[2]])       
-        }
+        # Replace `t` with Time
+        selected[[2]] <- gsub("^t", "Time", selected[[2]])
+        
+        # Replace `f` with Frequency
+        selected[[2]] <- gsub("^f", "Frequency", selected[[2]])       
         
         # Acceleration should not be abbreviated
         selected[[2]] <- gsub("Acc", "Acceleration", selected[[2]])
@@ -104,8 +84,17 @@ getFeatureNames <- function(type = "time", uciHarDataset = "UCI HAR Dataset") {
         # No need for hyphens
         selected[[2]] <- gsub("-", "", selected[[2]])
         
-        # Capatalize Mean
+        # Capitalize Mean
         selected[[2]] <- gsub("mean", "Mean", selected[[2]])
+        
+        # Replace BodyBody with Body
+        # https://www.coursera.org/learn/data-cleaning/peer/FIZtT/getting-and-cleaning-data-course-project/discussions/threads/yD2gtalxEeelRgqEwi0dZA
+        selected[[2]] <- gsub("BodyBody", "Body", selected[[2]])
+        
+        # Put a hyphen before the last X,Y or Z
+        selected[[2]] <- gsub("X$", "-X", selected[[2]])
+        selected[[2]] <- gsub("Y$", "-Y", selected[[2]])
+        selected[[2]] <- gsub("Z$", "-Z", selected[[2]])
         
         selected
 }
@@ -123,6 +112,53 @@ getActivityNames <- function(uciHarDataset = "UCI HAR Dataset") {
         activities
 }
 
-downloadData()
-getActivityNames()
-getMeanAndStandardDevData("UCI HAR Dataset", type="time", isTest=TRUE)
+averageDataSetsByActivityAndSubject <- function(dataFrame) {
+        newDataFrame <- data.frame(Subject = numeric(0), ActivityName = character(0))
+        subjects <- unique(dataFrame["Subject"])
+        subjects <- sort(subjects$Subject)
+        activities <- unique(dataFrame["ActivityName"])
+        features <- getCleanedFeatureNames()[[2]]
+
+        
+        for(id in subjects) {
+                for(name in activities) {
+                        filtered <- dataFrame[dataFrame["Subject"] == id & dataFrame["ActivityName"] == name,]
+                        new <- data.frame(Subject = id, ActivityName = name)
+                        for (feature in features) {
+                                count <- length(filtered[,feature])
+                                if (count > 0) {
+                                        new[feature] = mean(filtered[,feature])
+                                }else{
+                                        new[feature] = 0.0
+                                }
+                        }
+                        newDataFrame <- rbind(newDataFrame, new)
+                }
+        }
+        newDataFrame
+}
+
+# Loads the appropriate data (mean & std with appending activity type and subject value) for each of test and 
+# train is loaded and then combined using `rbind`
+mergeDataSets <- function() {
+        Data.test <- getMeanAndStandardDevData(isTest = TRUE)
+        Data.train <- getMeanAndStandardDevData(isTest = FALSE)
+        rbind(Data.test, Data.train)
+}
+
+# writes provided data frame to file with `write.table`
+exportDataSets <- function(dataFrame, filename = "UCI HAR Combined.txt") {
+        write.table(dataFrame, file = filename, row.names = FALSE)        
+}
+
+# Step 1. Download the data and unzip
+downloadDataAndUnzip()
+
+# Step 2. Merge data for `test` and `train`
+dataFrame <- mergeDataSets()
+
+# Optional Step 3. Write merged dataset to file
+# exportDataSets(dataFrame)
+
+# Step 4 (Step 5 from instructions)
+averageDataSetsByActivityAndSubject(dataFrame)
